9c81b27058be87d344b5c55dc74484ef8e44918a
tcollin <thibaut.collin2406@gmail.com>
2017-01-02 14:20:41 +0100

    Correction des liens
    - v_header : j'ai adapté le lien du css à la racine du projet
    - liste des taches : j'ai ajouté une doc pour faire un peut les
    problèmes de chacun et les taches à faire
    - v_accueil : j'ai mis les sujets dans une boucle il faudra remplacer
    par des données de la bdd + j'ai mis un overflow donc la liste des
    sujets défilera et non la page
    - v_subject : même système que v_accueil

--- src/vue/v_accueil.php
+++ src/vue/v_accueil.php
@@ -2,32 +2,35 @@
 
     <div class="container">
         <div class="title">Bienvenue sur le forum !</div>
-        <div class="table-responsive">
-            <table class="table table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th>
-                            <h4>Sujet</h4></th>
-                        <th>
-                            <h4>Catégorie</h4></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><b><a href="subject/">Mon téléviseur ne s'allume plus</a></b></td>
-                        <td><a href="#">Téléviseur</a></td>
-                    </tr>
-                    <tr>
-                        <td><b><a href="#">Quel forfait téléphone choisir ?</a></b></td>
-                        <td><a href="#">Téléphone</a></td>
-                    </tr>
-                    <tr>
-                        <td><b><a href="#">Quel smartphone choisir ?</a></b></td>
-                        <td><a href="#">Téléphone</a>, <a href="#">Mobile</a></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <hr>
+        <main>
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>
+                                <h4>Sujet</h4></th>
+                            <th>
+                                <h4>Catégorie</h4></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+            $nombredesujet = 5;
+            $i = 1;
+        
+        while ($i<$nombredesujet) { ?>
+                            <tr>
+                                <td><b><a href="subject/">Sujet
+                                            <?php echo $i;?></a></b></td>
+                                <td><a href="#">Catégorie <?php echo $i;?></a></td>
+                            </tr>
+                            <?php $i++;
+        } ?>
+                    </tbody>
+                </table>
+            </div>
+        </main>
     </div>
     </div>
 
--- src/vue/v_subject.php
+++ src/vue/v_subject.php
@@ -1,28 +1,45 @@
 <?php include('v_header.php'); ?>
 
     <div class="container">
-        <button type="submit" class="btn btn-success">Retour</button>
-        <div class="table-responsive">
-            <table class="table table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th>
-                            <div class="user">
-                                <div class="userimg"></div>
-                                <div class="usertext">Aleatwar - Mon téléviseur ne s'allume plus </div>
-                            </div>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <a href="../">
+            <button class="btn btn-success">Retour</button>
+        </a>
+
+        <hr>
+
+        <main>
+            <?php 
+            $nombredemessage = 5;
+            $i = 1;
+        
+        while ($i<$nombredemessage) { ?>
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>
+                                    <div class="user">
+                                        <div class="userimg"></div>
+                                        <div class="usertext">Titre sujet
+                                            <br />User
+                                            <?php echo $i;?>
+                                        </div>
+                                    </div>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <?php $i++;
+        } ?>
+        </main>
     </div>
     </div>
 
