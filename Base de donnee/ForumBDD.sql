33a240ebbf852dc5e03e2eb4dc603f12f28fa16a
aleatwar <aleatwar2512@gmail.com>
2017-01-09 20:02:45 +0100

    Base de donnée forum corrigée + Inserts
    Base de donnée sans erreur, avec inserts;
    Supression de la table DATE inutile;
    Ajout d'une clé étrangere id_personne dans sujet (pour identifier
    l'auteur d'un sujet sans avoir à regarder le premier post);

--- Base de donnee/ForumBDD.sql
+++ Base de donnee/ForumBDD.sql
@@ -5,11 +5,16 @@ DROP TABLE IF EXISTS ROLE ;
 DROP TABLE IF EXISTS CATEGORIE ;
 DROP TABLE IF EXISTS DATE ;
 
+
+-- Rang de l'user (admin, user, etc...)
+
 CREATE TABLE ROLE (
 role_id INT(11)  AUTO_INCREMENT NOT NULL,
 role_libelle VARCHAR(20),
 PRIMARY KEY (role_id) ) ENGINE=InnoDB;
 
+
+-- Table des utilisateurs
 
 CREATE TABLE PERSONNE (
 personne_id INT(11)  AUTO_INCREMENT NOT NULL,
@@ -19,25 +24,24 @@ personne_mail VARCHAR(250),
 role_id INT(11) NOT NULL,
 PRIMARY KEY (personne_id) ) ENGINE=InnoDB;
 
+-- Categories de sujets
 
 CREATE TABLE CATEGORIE (
 categorie_id INT(11)  AUTO_INCREMENT NOT NULL,
 categorie_nom VARCHAR(30),
 PRIMARY KEY (categorie_id) ) ENGINE=InnoDB;
 
+-- Sujets des forum crées par les utilisateurs
 
 CREATE TABLE SUJET (
 sujet_id INT(11)  AUTO_INCREMENT NOT NULL,
 sujet_titre VARCHAR(60),
+personne_id int(11),
 sujet_rang INT(2),
 categorie_id INT(11) NOT NULL,
 PRIMARY KEY (sujet_id) ) ENGINE=InnoDB;
 
-
-CREATE TABLE DATE (
-post_date DATETIME NOT NULL,
-PRIMARY KEY (post_date) ) ENGINE=InnoDB;
-
+-- Table des posts
 
 CREATE TABLE POST (
 sujet_id INT(11) NOT NULL,
@@ -52,10 +56,9 @@ CONSTRAINT UK_post_id UNIQUE (post_id)   ) ENGINE=InnoDB;
 
 ALTER TABLE PERSONNE ADD CONSTRAINT FK_PERSONNE_role_id FOREIGN KEY (role_id) REFERENCES ROLE (role_id);
 ALTER TABLE SUJET ADD CONSTRAINT FK_SUJET_categorie_id FOREIGN KEY (categorie_id) REFERENCES CATEGORIE (categorie_id);
+ALTER TABLE SUJET ADD CONSTRAINT FK_SUJET_personne_id FOREIGN KEY (personne_id) REFERENCES PERSONNE (personne_id);
 ALTER TABLE POST ADD CONSTRAINT FK_POST_personne_id FOREIGN KEY (personne_id) REFERENCES PERSONNE (personne_id);
-ALTER TABLE POST ADD CONSTRAINT FK_POST_date FOREIGN KEY (post_date) REFERENCES DATE (post_date);
 ALTER TABLE POST ADD CONSTRAINT FK_POST_sujet_id FOREIGN KEY (sujet_id) REFERENCES SUJET (sujet_id);
-
 
 -- insertions
 
@@ -70,30 +73,30 @@ insert into PERSONNE values (4,'alice','alice','alice@gmail.com',2);
 insert into CATEGORIE values (1,'divers');
 insert into CATEGORIE values (2,'moderation');
 
-insert into SUJET values (1, 'Premier sujet MONOLOGUE',2,1);
-insert into SUJET values (2, 'Second sujet DISCUSSION',2,1);
-insert into SUJET values (3, 'Topic de modération, à épingler',1,2);
+insert into SUJET values (1,'Premier sujet MONOLOGUE',1,2,1);
+insert into SUJET values (2,'Second sujet DISCUSSION',2,2,1);
+insert into SUJET values (3,'Topic de modération, à épingler',1,1,2);
 
 -- insertion sujet monologue
 
-insert into POST values (1,NOW(),1,1,'Bonjour ceci est mon premier message a moi même');
-insert into POST values (1,NOW(),1,2,'Et je me répond comme cela');
-insert into POST values (1,NOW(),1,3,'Je continue une troisième fois de me parler');
-insert into POST values (1,NOW(),1,4,'Quatrième ligne à moi même');
-insert into POST values (1,NOW(),1,5,'Et enfin la fin de mon monologue');
+insert into POST values (1,'2010-04-02 15:28:22',1,1,'Bonjour ceci est mon premier message a moi même');
+insert into POST values (1,'2010-04-02 15:29:58',1,2,'Et je me répond comme cela');
+insert into POST values (1,'2010-04-02 15:40:10',1,3,'Je continue une troisième fois de me parler');
+insert into POST values (1,'2010-04-02 16:50:12',1,4,'Quatrième ligne à moi même');
+insert into POST values (1,'2016-04-02 15:28:22',1,5,'Et enfin la fin de mon monologue');
 
 -- insertion sujet discussion
 
-insert into POST values (2,NOW(),2,5,'Bonjour ceci est le premier message de la discussion');
-insert into POST values (2,NOW(),3,6,'bonjour Aurore, comment ça va ?');
-insert into POST values (2,NOW(),4,7,'Coucou moi je suis Alice');
-insert into POST values (2,NOW(),2,8,'Bonjour Alice');
-insert into POST values (2,NOW(),4,9,'A bientôt les ptizamis !');
+insert into POST values (2,'2010-04-02 15:28:22',2,6,'Bonjour ceci est le premier message de la discussion');
+insert into POST values (2,'2011-04-02 15:29:58',3,7,'bonjour Aurore, comment ça va ?');
+insert into POST values (2,'2011-04-02 15:29:58',4,8,'Coucou moi je suis Alice');
+insert into POST values (2,'2011-04-05 00:50:12',2,9,'Bonjour Alice');
+insert into POST values (2,'2017-01-02 07:45:23',4,10,'A bientôt les ptizamis !');
 
 -- insertion sujet moderation
 
-insert into POST values (3,NOW(),1,9,'Premier message dans le sujet Moderation');
-insert into POST values (3,NOW(),2,10,'Il faudrait epingler ce topic');
-insert into POST values (3,NOW(),3,11,'Je suis tout à fait daccord');
-insert into POST values (3,NOW(),1,12,'le topic a epingler a le rang numero 1');
-insert into POST values (3,NOW(),1,13,'Et les topics normaux ont un rang numero 2');
+insert into POST values (3,'2017-01-02 07:45:23',1,11,'Premier message dans le sujet Moderation');
+insert into POST values (3,'2017-01-02 07:45:23',2,12,'Il faudrait epingler ce topic');
+insert into POST values (3,'2017-01-02 07:45:23',3,13,'Je suis tout à fait daccord');
+insert into POST values (3,'2017-01-04 11:35:36',1,14,'le topic a epingler a le rang numero 1');
+insert into POST values (3,'2017-01-07 16:21:56',1,15,'Et les topics normaux ont un rang numero 2');
